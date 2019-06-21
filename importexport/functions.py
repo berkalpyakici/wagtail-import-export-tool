df5dbb88d56547eee14d37ff0849f7da4af19034
Berk Alp Yakici <berkalp.y@gmail.com>
2019-06-21 09:51:00 -0500

    Add definitions for functions & tests

---
+++ importexport/functions.py
@@ -0,0 +1,36 @@
+def null_pks():
+    """
+    Nullifies primary keys within all supplied fields.
+
+    Arguments:
+    fields -- Dictionary of all fields.
+
+    Returns:
+    N/A. Overwrites the argument.
+    """
+    pass
+
+def null_fks():
+    """
+    Nullifies foreign keys within all supplied fields.
+
+    Arguments
+    fields -- Dictionary of all fields.
+
+    Returns:
+    N/A. Overwrites the argument.
+    """
+    pass
+
+def zip_contents():
+    """
+    Creates and returns a zip archive of all supplied items.
+    """
+    pass
+
+def unzip_contents():
+    """
+    Extracts all items in the zip archive and returns a mapping
+    of the contents, as well as their location in tempdir.
+    """
+    pass
