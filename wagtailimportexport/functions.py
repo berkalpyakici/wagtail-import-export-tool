2929c4dfb2649cb13b959dd0bcda016bf164000b
Berk Alp Yakici <berkalp.y@gmail.com>
2019-06-25 13:04:34 -0500

    Add PK and FK nullifying

--- wagtailimportexport/functions.py
+++ wagtailimportexport/functions.py
@@ -6,6 +6,10 @@ import json
 
 from django.core.files.storage import get_storage_class
 from django.core.serializers.json import DjangoJSONEncoder
+from django.db.models.fields.related import ForeignKey
+from django.db.models.fields.reverse_related import ManyToOneRel
+
+from wagtail.core.fields import StreamField
 
 
 def null_pks(page, data):
@@ -13,25 +17,122 @@ def null_pks(page, data):
     Nullifies primary keys within all supplied fields.
 
     Arguments:
-    fields -- Dictionary of all fields.
+    page -- Page object.
+    data -- Page object in dictionary format.
 
     Returns:
     N/A. Overwrites the argument.
     """
-    pass
+    
+    # Nullify the main ID
+    data['id'] = None
+    data['pk'] = None
+
+    # Loop through all fields.
+    for field_name, field_val in data.items():
+        if type(field_val) != list:
+            continue
+        
+        for i, sub_item in enumerate(field_val):
+            if 'pk' in sub_item:
+                data[field_name][i]['pk'] = None
+
+def find_null_child_blocks(subfield, location, data):
+    """
+    Recursive function to find all children blocks
+    within streamfield and nullify fks.
+
+    Arguments:
+    subfield -- A field.
+    location -- (Ordered) list of field keys that act
+                as a tree.
+    data -- Data object to overwrite the changes to.
+
+    Returns:
+    N/A. Overwrites data.
+    """
+
+    # Some fields do not have child_blocks, and we should not
+    # investigate further if that's the case.
+    if not "child_blocks" in subfield.__dict__.keys():
+        continue
+
+    # Go through all fields.
+    for field_key, field_val in subfield.child_blocks.items():
+
+        # We want to catch the ForeignKey
+        if isinstance(field_val, ForeignKey):
+            #TODO: Implement overwriting.
+            print(field_key, type(field_val), location)
+        
+        # Recursive Calls
+        find_null_child_blocks(field_val, location+[field_key], data)
+
+def find_null_child_relations(subfield, location, data):
+    """
+    Recursive function to find all children relations
+    within manyotoone relationships and nullify fks.
+
+    Arguments:
+    subfield -- A field.
+    location -- (Ordered) list of field keys that act
+                as a tree.
+    data -- Data object to overwrite the changes to.
+
+    Returns:
+    N/A. Overwrites data.
+    """
+
+    # Some fields do not have related_model, and we should not
+    # investigate further if that's the case.
+    if not "related_model" in subfield.__dict__.keys():
+        continue
+
+    # Go through all fields.
+    for field in subfield.related_model._meta.fields:
+
+        # We want to catch the ForeignKey
+        if isinstance(field, ForeignKey):
+            if not location[0] in data:
+                continue
+            
+            for i, value in enumerate(data[location[0]]):
+                if not field.name in data[location[0]][i]:
+                    continue
+                
+                data[location[0]][i][field.name] = None
 
 def null_fks(page, data):
     """
     Nullifies foreign keys within all supplied fields.
 
-    Arguments
-    fields -- Dictionary of all fields.
+    Arguments:
+    page -- Page object.
+    data -- Page object in dictionary format.
 
     Returns:
     N/A. Overwrites the argument.
     """
-    pass
 
+    # Loop through all fields.
+    for field in page._meta.get_fields():
+        
+        # Check whether the field is a ForeignKey.
+        # By nature, owner, content_type, live_revision
+        # are foreign keys defined by wagtail core pages.
+        if(isinstance(field, ForeignKey)):
+            data[field.name] = None
+
+        # StreamFields often have foreign keys associated with them.
+        # TODO: Make sure that we can catch all foreign keys within streamblocks.
+        if(isinstance(field, StreamField)):
+            find_null_child_blocks(field.stream_block, [field.name], data)
+
+        # Many to One relations often have foreign keys associated with them.
+        # TODO: Make sure that we can catch all foreign keys within streamblocks.
+        if(isinstance(field, ManyToOneRel)):
+            find_null_child_relations(field, [field.name], data)
+        
 def zip_contents(page_contents):
     """
     Creates and returns a zip archive of all supplied items.
